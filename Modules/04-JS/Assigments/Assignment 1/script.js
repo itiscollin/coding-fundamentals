81747e3e0d553e350d06256eeefc42068f50a643
Collin <collin.estlee@gmail.com>
2023-06-14 20:00:10 +0800

    Add files via upload

--- Modules/04-JS/Assigments/Assignment 1/script.js
+++ Modules/04-JS/Assigments/Assignment 1/script.js
@@ -1,14 +1,17 @@
 // Given an array of integers, find the sum of all integers. 
 // You can be assure that the array will only contain integers (both positive and negative).
 
-const num = [21, 1, -22]
-let length = num.length -1
-let total = parseInt(0); 
-
-for (i = 0; i <= length; i++){
-    total = total + parseInt(num.pop())
+function sum(numArray){
+    let length = numArray.length -1
+    let total = parseInt(0); 
+    
+    for (i = 0; i <= length; i++){
+        total = total + parseInt(numArray.pop())
+        
+    }
+    return total
 }
 
 
-
-console.log (total)
+console.log(sum([1, 2, 3, 4]) === 10)
+console.log(sum([-3, 5, 19, -6]) === 15)
