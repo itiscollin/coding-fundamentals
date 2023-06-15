1b8ed1fff86f4d787d3ec467e010350cce429740
Collin <collin.estlee@gmail.com>
2023-06-15 14:42:13 +0800

    Add files via upload
    JS Assignment 4 - Tetrahedral number

---
+++ Modules/04-JS/Assigments/Assignment 4/script.js
@@ -0,0 +1,13 @@
+
+function tetra(n){  
+ return  n * (n+1)*(n+2)/6
+ 
+}
+
+console.log(tetra(2) === 4)
+console.log(tetra(5) === 35)
+console.log(tetra(6) === 56)
+
+console.log(tetra(2))
+console.log(tetra(5))
+console.log(tetra(6))
