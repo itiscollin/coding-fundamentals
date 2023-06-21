aabc10b20558f03aa0a4cf5512422f1b4652f43b
Collin <collin.estlee@gmail.com>
2023-06-21 15:50:45 +0800

    Add files via upload
    Code fix

--- Modules/05-Computational-Thinking/Assigments/Assignment 1/script.js
+++ Modules/05-Computational-Thinking/Assigments/Assignment 1/script.js
@@ -2,6 +2,11 @@
 // But A cannot go backward
 
 function possibleBonus(possitionA, possitionB){
+
+    if(possitionA < 0|| possitionB < 0){
+        return false
+    }
+
     const distance = possitionB - possitionA;
 
     if(distance > 6 || distance < 1){
@@ -15,9 +20,11 @@ function possibleBonus(possitionA, possitionB){
 
 // Test
 
-console.log((possibleBonus(3,7) === true))
-console.log(possibleBonus(1,9) === true)
-console.log(possibleBonus(5,3) === true)
+console.log(possibleBonus(0,0) === true)
+
+// console.log((possibleBonus(3,7) === true))
+// console.log(possibleBonus(1,9) === true)
+// console.log(possibleBonus(5,3) === true)
 // possibleBonus(3, 7) ➞ true
 
 // possibleBonus(1, 9) ➞ false
